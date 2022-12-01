a5addc6e32dccc30a742fda48a0893a94825c383
xlan11 <alankelly@gmail.com>
2022-12-01 16:37:28 +0000

    complete 5

--- js/kanban.js
+++ js/kanban.js
@@ -18,7 +18,7 @@ add_btn.addEventListener('click', () => {
 const create_item = () => {
   let item = document.createElement("div");
   item.classList.add("item");
-  item.setAttribute("id", `item-${order}`);
+  item.id = `item-${order}`;
   item.draggable = true;
   item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id));
   item.addEventListener('dragend', event => event.dataTransfer.clearData());
