402e8e7f56f3bffe7208c9e90a4f91908e865f66
xlan11 <alankelly@gmail.com>
2022-12-01 16:14:52 +0000

    validate input

--- js/kanban.js
+++ js/kanban.js
@@ -22,10 +22,22 @@ const create_item = () => {
   item.draggable = true;
   item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id));
   item.addEventListener('dragend', event => event.dataTransfer.clearData());
+  
   let input = document.createElement("input");
   item.appendChild(input);
+
   let save_btn = document.createElement("button");
-  save_btn.innerHTML=("Save");
+  save_btn.innerHTML = "Save";
+  save_btn.addEventListener(click, () => {
+    error.innerHTML = "";
+    if(
+      input.value !== "") {
+        order += 1;
+        item.innerHTML = input.value;
+        error.innerHTML = message;
+      }
+  })
+
 };
 
 document.querySelectorAll('.drop').forEach(element => {
